6ca0c63e073ecb2aa4fce26d333a9a29e252ca38
Romain <romain.kruszewski@campus-igs-toulouse.fr>
2019-07-10 12:50:46 +0200

    maven

--- src/main/java/bucketplace/Main.java
+++ src/main/java/bucketplace/Main.java
@@ -1,10 +1,16 @@
 package bucketplace;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
 
-	public static void main(String[] args) {
-		System.out.println("Bonjour bienvenu au Bucket Place");
+	private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+	private static final String LOG_MSG_START = "Application launched";
 
+	public static void main(String[] args) {
+		LOG.info(LOG_MSG_START);
+		System.out.println("Bonjour bienvenu au Bucket Place");
 	}
 
 }
